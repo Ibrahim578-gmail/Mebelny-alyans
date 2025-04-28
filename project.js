29703078f071b220bbdbe5a717ac825cdc12c7fe
Ibrahim578-gmail <ibrahimchebupelka228@gmail.com>
2025-04-28 17:03:57 +0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,21 @@
+var num_1 = 40;
+var num_2 = 15;
+console.log("Вычитание: " + (num_1 - num_2));
+
+var num_3 = 12;
+num_3 = num_3 ++;
+console.log(num_3);
+
+console.log(Math.PI);
+console.log(Math.min(1,2,4,6,3,9));
+
+var var_if = true;
+var var_if2 = false; 
+    if(var_if == var_if2){
+        console.log("ok");}
+        else if(var_if == true)
+            console.log("so good");
+            
+else{  
+    console.log("eror");
+}
